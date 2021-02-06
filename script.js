d5c595c69eba76d57c71b1671daf204b0a990d57
gokhan <gkhncm32@gmail.com>
2021-02-06 19:07:18 +0300

    many changes

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var mymap = L.map('mapid').setView([52.230533, 21.006266], 10);
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri'
 }).addTo(mymap);
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
